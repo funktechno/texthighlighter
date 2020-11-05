69cde08bec62c5738d37dd8a0ed478ff85f953e0
Taylor Funk <theefunk@gmail.com>
2020-11-05 14:21:28 -0500

    working example

--- dist/TextHighlighter.js
+++ dist/TextHighlighter.js
@@ -1,6 +1,22 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// TextHighLighterv2 client
+var index_1 = require("../src/index");
+// declare const TextHighlighter: TextHighlighterType;
+var TextHighlighter = {
+    doHighlight: index_1.doHighlight,
+    deserializeHighlights: index_1.deserializeHighlights,
+    serializeHighlights: index_1.serializeHighlights,
+    removeHighlights: index_1.removeHighlights,
+    optionsImpl: index_1.optionsImpl
+};
+window.TextHighlighter = TextHighlighter;
+// globalThis/* or window */.TextHighlighter = TextHighlighter;
+
+},{"../src/index":3}],2:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.paramsImp = exports.optionsImpl = exports.activator = exports.groupHighlights = exports.defaults = exports.highlightI = exports.haveSameColor = exports.unique = exports.sortByDepth = exports.refineRangeBoundaries = exports.dom = exports.IGNORE_TAGS = exports.NODE_TYPE = exports.TIMESTAMP_ATTR = exports.DATA_ATTR = void 0;
 // eslint-disable-next-line @typescript-eslint/class-name-casing
 var highlightI = /** @class */ (function () {
@@ -435,7 +451,7 @@ function sortByDepth(arr, descending) {
 }
 exports.sortByDepth = sortByDepth;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.highlightRange = exports.createWrapper = exports.optionsImpl = exports.removeHighlights = exports.serializeHighlights = exports.deserializeHighlights = exports.doHighlight = void 0;
@@ -947,4 +963,4 @@ var removeHighlights = function (element, options) {
 };
 exports.removeHighlights = removeHighlights;
 
-},{"./TextHighlighterUtils":1}]},{},[2]);
+},{"./TextHighlighterUtils":2}]},{},[1]);
